d608efee60245394457e2ddabd449710d73d5af8
owl1753 <bighammer1753@gmail.com>
2021-04-02 17:08:34 +0900

    0402

---
+++ GP_0402/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Enemy enemy;
+    public GameObject enemyGroup;
+    private bool isPlaying = true;
+
+    public void OffSpawner()
+    {
+        isPlaying = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying)
+        {
+            int spawnPer = Random.Range(0, 1);
+            if (spawnPer < 10)
+            {
+                Enemy e = Instantiate(enemy);
+
+                float posY = Random.Range(-2.65f, 2.65f);
+                int isLeftInstantiate = Random.Range(0, 2);
+
+                if (isLeftInstantiate == 0)
+                {
+                    e.transform.position = new Vector3(-8.7f, posY, -1);
+                    e.transform.rotation = new Quaternion(0, 0, 0, 0);
+                    e.SetdirectionVector(1);
+                }
+                else
+                {
+                    e.transform.position = new Vector3(8.7f, posY, -1);
+                    e.transform.rotation = new Quaternion(0, 0, 180, 0);
+                    e.SetdirectionVector(-1);
+                }
+                e.transform.parent = enemyGroup.transform;
+            }
+        }
+    }
+}
---
+++ GP_0402/Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int hp = 3;
+    public int speed = 10;
+
+    public int GetHP()
+    {
+        return hp;
+    }
+
+    public void SetHP(int hp)
+    {
+        this.hp = hp;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp > 0 )
+        {
+            Vector3 posInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+            Vector3 pos = transform.position;
+
+            pos += posInput * Time.deltaTime * speed;
+
+            if (pos.x < -4.68)
+            {
+                pos.x = -4.68f;
+            }
+            else if (pos.x > 4.68)
+            {
+                pos.x = 4.68f;
+            }
+            if (pos.y < -2.65)
+            {
+                pos.y = -2.65f;
+            }
+            else if (pos.y > 2.65)
+            {
+                pos.y = 2.65f;
+            }
+
+            transform.position = pos;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            hp -= 1;
+            print(hp);
+        }
+    }
+}
